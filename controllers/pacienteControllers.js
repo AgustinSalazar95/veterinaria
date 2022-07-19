5a8a1ff32dd8b162005a68405a0632575aa60640
AgusSalazar <agustin.salazar@openix.com.ar>
2022-07-19 16:49:41 -0300

    Edit and Delete Pacientes

--- controllers/pacienteControllers.js
+++ controllers/pacienteControllers.js
@@ -17,15 +17,66 @@ const obtenerPacientes = async (req,res) => {
 }
 
 const obtenerPaciente = async (req,res) =>{
-    
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+    //como son objetId son distintos entonces los transformamos en string
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msg: 'Accion no valida'});
+    }
+
+    if(paciente){
+        res.json(paciente);
+    }
+
 }
 
 const actualizarPaciente = async (req,res) =>{
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: 'No Encontrado'})
+    }
+    //como son objetId son distintos entonces los transformamos en string
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msg: 'Accion no valida'});
+    }
+
+    //Actualizar Paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado =  await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
 const eliminarPaciente = async (req,res) =>{
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
 
+    if(!paciente){
+        return res.status(404).json({msg: 'No Encontrado'})
+    }
+    //como son objetId son distintos entonces los transformamos en string
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msg: 'Accion no valida'});
+    }
+
+    try {
+        //como ya teneoms el paciente a eliminar usamos deleteOne
+        await paciente.deleteOne();
+        res.json({msg: 'Paciente Eliminado'});
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export {
