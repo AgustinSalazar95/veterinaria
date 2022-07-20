4ebcaed87dd6803f91c8e60dfe30f15c66a2bc63
AgusSalazar <agustin.salazar@openix.com.ar>
2022-07-20 16:28:10 -0300

    Login Formulario

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom'
 const AuthLayout = () => {
   return (
     <>
-        <h1>Desde Auth Layout</h1>
-        {/* para mostrar el contenido de Login */}
-        <Outlet />
-        
+        {/* para mostrar el contenido de Login, md(media queries) */}
+        <main className='container mx-auto md:grid md:grid-cols-2 mt-12 ml-12 gap-12 p-5 items-center'>
+            <Outlet />
+        </main>
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,49 @@
+import { Link } from 'react-router-dom';
+
 const Login = () => {
   return (
     <>
-        <h2>Login</h2>
+        <div>
+            <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span></h1>
+        </div>
+        <div>
+            <from>
+                <div className="my-5">
+                    <label className="uppercase text-gray-600 block text-xl font-bold">
+                        Email
+                    </label>
+                    <input 
+                        type="email"
+                        placeholder="Email de Registro"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    />
+                </div>
+                <div className="my-5">
+                    <label className="uppercase text-gray-600 block text-xl font-bold">
+                        Password
+                    </label>
+                    <input 
+                        type="password"
+                        placeholder="Ingrese su Password"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    />
+                </div>
+                <input 
+                    type='submit'
+                    value='Iniciar Sesión'
+                    className='bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto'
+                />
+            </from>
+            <nav className='mt-5 lg:flex lg:justify-between'>
+                {/* en vez de <a> utilizamos Link */}
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/registrar">¿No tienes una cuenta? Registrate</Link>
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/olvide-password">Olvide mi Password</Link>
+            </nav>
+        </div>
     </>
   )
 }
