95e35e5e4f056c869db69e9cb657073850360a3a
AgusSalazar <agustin.salazar@openix.com.ar>
2022-07-20 16:53:32 -0300

    Pages diseños olvidepass, registrar, crear cuenta

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,9 @@ const Login = () => {
   return (
     <>
         <div>
-            <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span></h1>
+            <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra {" "}<span className='text-black'>tus Pacientes</span></h1>
         </div>
-        <div>
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
             <from>
                 <div className="my-5">
                     <label className="uppercase text-gray-600 block text-xl font-bold">
