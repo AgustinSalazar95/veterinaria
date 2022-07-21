de5b29f0a2fcc9d897c012fa5e5dfedf30f83b54
AgusSalazar <agustin.salazar@openix.com.ar>
2022-07-21 17:29:19 -0300

    Ingreso Usuario

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,25 @@ import OlvidePassword from './pages/OlvidePassword';
 import Registrar from './pages/Registrar';
 import ConfirmarCuenta from './pages/ConfirmarCuenta';
 import NuevoPassword from './pages/NuevoPassword';
+
+import { AuthProvider } from './context/AuthProvider';
+
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        {/* para definir la pagina principal  pasar un porps index*/}
-          <Route path='/' element={<AuthLayout />}>
-              <Route index element={<Login />}/>
-              <Route path='registrar' element={<Registrar />} />
-              <Route path='olvide-password' element={<OlvidePassword />} />
-              <Route path='olvide-password/:token' element={<NuevoPassword />} />
-              <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-          </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* para definir la pagina principal  pasar un porps index*/}
+            <Route path='/' element={<AuthLayout />}>
+                <Route index element={<Login />}/>
+                <Route path='registrar' element={<Registrar />} />
+                <Route path='olvide-password' element={<OlvidePassword />} />
+                <Route path='olvide-password/:token' element={<NuevoPassword />} />
+                <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+            </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
