da9bc22f0303f29bf3678548f2c273f4447413e1
AgusSalazar <agustin.salazar@openix.com.ar>
2022-07-25 14:40:23 -0300

    Inician Layout Pacientes

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -5,11 +5,15 @@ const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
+    const [cargando, setCargando] = useState(true);
     const [auth, setAuth] = useState({});
     useEffect(() => {
         const autenticarUsuario = async () =>{
             const token = localStorage.getItem('token');
-            if(!token) return
+            if(!token){
+                setCargando(false);
+                return
+            }
 
             const config = {
                 headers: {
@@ -25,16 +29,24 @@ const AuthProvider = ({children}) => {
                 console.log(error.response.data.msg);
                 setAuth({})
             }
+            setCargando(false);
 
         }
         autenticarUsuario();
     }, [])
 
+    const cerrarSesion = () => {
+        localStorage.removeItem('token');
+        setAuth({})
+    }
+
     return(
         <AuthContext.Provider
             value={{
                 auth,
-                setAuth
+                setAuth,
+                cargando,
+                cerrarSesion
             }}
         >
             {children}    
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,8 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [alerta, setAlerta] = useState({});
 
+    const { setAuth} = useAuth();
+
     const navigate = useNavigate();
 
     const handleSubmit = async e =>{
@@ -23,9 +25,8 @@ const Login = () => {
         }
         try {
             const {data} = await clienteAxios.post('/veterinarios/login', {email, password});
-            
             localStorage.setItem('token', data.token);
-
+            setAuth(data);
             navigate('/admin');
         } catch (error) {
             setAlerta({
